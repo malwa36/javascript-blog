7582478a10bd64d176984456af331e4591fdcba4
Malwina Leśniewska <malwinalesniewska@gazeta.pl>
2022-04-27 13:43:02 +0200

    add class active to the correct article

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@ function titleClickHandler(event){
   console.log('targetArticle')
 
   /* [IN PROGRESS] add class 'active' to the correct article */
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
