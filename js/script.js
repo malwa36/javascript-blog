1d13c78e2fefc2efc86752b6499148ed1aa40c7b
Malwina Leśniewska <malwinalesniewska@gazeta.pl>
2022-05-04 11:07:04 +0200

    index.html was modified: remove <li> from each post-tags

--- js/script.js
+++ js/script.js
@@ -45,6 +45,7 @@ function titleClickHandler(event){
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
+const optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks(customSelector = ''){
   console.log('Title Lists are just generated');
@@ -86,3 +87,31 @@ function generateTitleLinks(customSelector = ''){
 }
 
 generateTitleLinks();
+
+function generateTags(){
+  /* find all articles */
+
+  /* START LOOP: for every article: */
+
+    /* find tags wrapper */
+
+    /* make html variable with empty string */
+
+    /* get tags from data-tags attribute */
+
+    /* split tags into array */
+
+    /* START LOOP: for each tag */
+
+      /* generate HTML of the link */
+
+      /* add generated code to html variable */
+
+    /* END LOOP: for each tag */
+
+    /* insert HTML of all the links into the tags wrapper */
+
+  /* END LOOP: for every article: */
+}
+
+generateTags();
